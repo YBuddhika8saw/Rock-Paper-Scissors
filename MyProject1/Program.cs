203e9aa3761cc4ccdd1e318da3895307f6aea4c4
YBuddhika8saw <2020is018@stu.ucsc.cmb.ac.lk>
2023-11-11 09:16:43 +0530

    Add project files.

---
+++ MyProject1/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Random random = new Random();
+        bool playAgain = true;
+        String Input;
+        while(playAgain)
+        {   
+            int val =random.Next(0,3);
+            Console.WriteLine("Input Rock, Paper Or Sessior ");
+            Input = Convert.ToString(Console.ReadLine());
+
+        }
+
+              Console.ReadKey();
+    }
+}
